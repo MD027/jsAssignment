e911e8d76844dc64890af949ef14ec1d5852e4f7
Parmar <mohitkumar-bipinbhai.parmar@capgemini.com>
2022-05-16 12:30:10 +0530

    assignment

---
+++ 11.js
@@ -0,0 +1,14 @@
+
+function func() {
+
+
+	var arr = ['Portal', 'Science',
+			'Computer', 'GeeksforGeeks'];
+	document.write(arr);
+
+
+	var new_arr = arr.reverse();
+	document.write("<br>");
+	document.write(new_arr);
+}
+func();

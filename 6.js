cc8f62a4e052f2a66a671269c35c45f7ba4be039
Parmar <mohitkumar-bipinbhai.parmar@capgemini.com>
2022-05-16 12:20:40 +0530

    assignment

---
+++ 6.js
@@ -0,0 +1,11 @@
+
+function myFunction(){
+    var listt=["hello","world","in","a","frame"];
+    console.log("********");
+    for(i=0;i<listt.length;i++){
+        console.log("*"+listt[i]+"*");
+    }
+    console.log("********");
+
+}
+myFunction();
---
+++ 9.js
@@ -0,0 +1,4 @@
+var array1 = ["Vijendra", "Singh"];
+var array2 = ["Singh", "Shakya"];
+
+console.log(array1.concat(array2));
